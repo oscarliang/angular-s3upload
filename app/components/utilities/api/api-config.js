ed0260669c9b626ee47195b572f487679a523f93
Oscar <oliang@acresta.com>
2017-04-01 00:14:23 +1100

    add

--- app/components/utilities/api/api-config.js
+++ app/components/utilities/api/api-config.js
@@ -1,7 +1,7 @@
-app.factory('config', function() { 
+app.factory('config', function() {
 	config = { //new update
                 projectsubject: 'Oscar Test',
-                api_url: 'http://10.10.10.10'
+                api_url: 'http://upload.oscartechnology.com'
 	};
         return config;
 });
